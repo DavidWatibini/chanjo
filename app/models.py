f22cc47ce33d6200e3dcbaa639462f1af20128a7
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-02 16:52:45 +0300

    register and login

--- app/models.py
+++ app/models.py
@@ -1,14 +1,27 @@
 from . import db
+from datetime import datetime
 
 
 class User(db.Model):
 
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key =True)
+    name = db.Column(db.String(100))
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     email = db.Column(db.String(255),unique = True,index = True)
+    password = db.Column(db.String(255), nullable=False)
 
+    def __repr__(self):
+        return f"{self.name}"
+
+
+class Meta(db.Model):
+
+    id = db.Column(db.Integer,primary_key =True)
+
+    def __repr__(self):
+        return f"{self.username}"
 
 class Role(db.Model):
 
@@ -19,3 +32,13 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
+
+class Drives(db.Model):
+    
+    id = db.Column(db.Integer,primary_key =True)
+    vaccination = db.Column(db.String,nullable=False)
+    date = db.Column(db.DateTime,nullable=False)
+    location = db.Column(db.String(255),nullable=False)
+
+    def __repr__(self):
+        return f"Drives {self.vaccination}"
